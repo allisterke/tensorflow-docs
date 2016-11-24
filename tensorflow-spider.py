528726dcf40ba3ce3baf8228a6fdb73ed16dcfa6
ally <allisterke@gmail.com>
2016-11-24 11:21:35 +0800

    fetch only master; add url filtering

--- tensorflow-spider.py
+++ tensorflow-spider.py
@@ -1,30 +1,29 @@
 import scrapy
 from scrapy.linkextractors.lxmlhtml import LxmlLinkExtractor 
 import os 
+import re
 
 #print scrapy.linkextractors.IGNORED_EXTENSIONS 
 
 NON_HTML_SUFFIX = scrapy.linkextractors.IGNORED_EXTENSIONS + ['js', 'ico']
-REWRITE_SUFFIX = ['get_started', 'tutorials', 'how_tos', 'api_docs', 'resources'] 
+REJECT_URL = r'/r0.11/|/r0.10/|/r0.9/|/r0.8/|/r0.7/|/0.6.0/|/code/'
+TENSORFLOW_DOMAIN = 'www.tensorflow.org'
 
 class QuotesSpider(scrapy.Spider):
     name = "tensorflow"
     start_urls = [
-        'https://www.tensorflow.org/',
+        'https://www.tensorflow.org/versions/master/get_started/index.html',
     ] 
 
     allowed_domains = [
-        'www.tensorflow.org'
+        TENSORFLOW_DOMAIN,
     ] 
 
-    parsed = set()
-    queue = [] 
+    def __init__(self):
+        self.parsed = set()
 
     def save(self, response):
-        if 'redirect_urls' in response.meta:
-            url = response.meta['redirect_urls'][0]
-        else:
-            url = response.url 
+        url = response.url
 
         path = url.split('/')
         path.pop(0)
@@ -37,9 +36,6 @@ class QuotesSpider(scrapy.Spider):
         fname = path.pop(-1)
         if not fname:
             fname = 'index.html'
-        elif fname in REWRITE_SUFFIX:
-            path.append(fname)
-            fname = 'index.html' 
 
         if path:
             path = domain + '/' + '/'.join(path)
@@ -54,6 +50,17 @@ class QuotesSpider(scrapy.Spider):
 
     def parse(self, response):
         url = response.url
+
+        if url in self.parsed:
+            return
+        else:
+            self.parsed.add(url)
+
+        # reject some url
+        match = re.search(REJECT_URL, url);
+        if match:
+            return
+
         print url 
 
         # save response content
@@ -65,15 +72,13 @@ class QuotesSpider(scrapy.Spider):
                 return
 
         # parse links
-        for link in LxmlLinkExtractor(allow_domains=self.allowed_domains, deny_extensions=('nothing'), tags=('a', 'link', 'img', 'script'), attrs=('href', 'src')).extract_links(response):
-            url = link.url
-            if url not in self.parsed:
-                self.parsed.add(url)
-                self.url = url
-#                   print url
-                yield scrapy.Request(url, callback=self.parse, errback=self.error) 
+        for link in LxmlLinkExtractor(deny=(REJECT_URL,), allow_domains=(TENSORFLOW_DOMAIN,), deny_extensions=('nothing',), tags=('a', 'link', 'img', 'script'), attrs=('href', 'src')).extract_links(response):
+            url = link.url 
+            self.url = url
+            yield scrapy.Request(url, callback=self.parse, errback=self.error) 
 
     def error(self, failure):
+        # try again
         scrapy.Request(self.url, callback=self.parse, errback=self.error)
         
 
